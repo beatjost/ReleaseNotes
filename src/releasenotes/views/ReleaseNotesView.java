a670a8db8e9b8d40fbf34f58cbea93bff5dbc572
Beat Jost <beat.jost@basenet.ch>
2015-03-20 10:55:32 +0100

    Check on startup for empty preference path.

--- src/releasenotes/views/ReleaseNotesView.java
+++ src/releasenotes/views/ReleaseNotesView.java
@@ -51,26 +51,13 @@ import releasenotes.Activator;
 import releasenotes.preferences.PreferenceConstants;
 import releasenotes.preferences.ReleaseNotePreferencePage;
 
-/**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model objects should be presented in the view. Each view can present the
- * same model objects using different labels and icons, if needed. Alternatively, a single label provider can be shared
- * between views in order to ensure that objects of the same type are presented in the same way everywhere.
- * <p>
- */
-
 public class ReleaseNotesView extends ViewPart {
 
 	/**
 	 * The ID of the view as specified by the extension.
 	 */
 	public static final String ID = "releasenotes.views.ReleaseNotesView";
+	private static final String NO_DIRECTORY_PATH_SET = "No directory path set in preferences!";
 
 	private TableViewer viewer;
 	private Action actionNew;
@@ -80,12 +67,8 @@ public class ReleaseNotesView extends ViewPart {
 	private Action actionEditFileName;
 	private Action actionDeleteFile;
 
-	/*
-	 * The content provider class is responsible for providing objects to the view. It can wrap existing
-	 * objects in adapters or simply return objects as-is. These objects may be sensitive to the current
-	 * input of the view, or ignore it and always show the same content (like Task List, for example).
-	 */
 	class ViewContentProvider implements IStructuredContentProvider {
+		
 		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
 		}
 
@@ -93,7 +76,12 @@ public class ReleaseNotesView extends ViewPart {
 		}
 
 		public Object[] getElements(Object parent) {
-			return getFileNames();
+			String path = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.P_PATH);
+			if (path.isEmpty()) {
+				return new Object[] {NO_DIRECTORY_PATH_SET};
+			} else {
+				return getFileNames();
+			}
 		}
 	}
 
@@ -118,8 +106,7 @@ public class ReleaseNotesView extends ViewPart {
 	}
 
 	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
+	 * This is a callback that will allow us  to create the viewer and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
 		viewer = new TableViewer(parent, SWT.SINGLE | SWT.H_SCROLL | SWT.V_SCROLL);
@@ -128,7 +115,6 @@ public class ReleaseNotesView extends ViewPart {
 		viewer.setSorter(new NameSorter());
 		viewer.setInput(getViewSite());
 
-		// Create the help context id for the viewer's control
 		makeActions();
 		hookContextMenu();
 		hookDoubleClickAction();
@@ -164,7 +150,7 @@ public class ReleaseNotesView extends ViewPart {
 		mgr.add(actionDeleteFile);
 		//mgr.add(new Separator());
 	}
-
+	
 	private void makeActions() {
 
 		// -------------------------------------------------------------------------------------------------------
@@ -275,6 +261,12 @@ public class ReleaseNotesView extends ViewPart {
 	private void openFile() {
 		ISelection selection = viewer.getSelection();
 		Object obj = ((IStructuredSelection) selection).getFirstElement();
+		
+		if (obj.toString().equals(NO_DIRECTORY_PATH_SET)) {
+			openPreferencePage();
+			return;
+		}
+		
 		StringBuilder filePath = new StringBuilder();
 		String path = Activator.getDefault().getPreferenceStore().getString(PreferenceConstants.P_PATH);
 		String fileName = obj.toString();
